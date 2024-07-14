5602ed3010c42a8e4a8debc0445739df072946fe
Alvaresan <alvaresan@icloud.com>
2024-07-14 10:32:45 +0200

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return <Button>Hello</Button>;
+export default function Page() {
+  return <div className="flex flex-col gap-y-4">
+    <h1>Dashboard</h1>
+    <UserButton />
+  </div>
 }
